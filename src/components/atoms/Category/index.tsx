3958a85c6f0f4ece14fa563e9790dccb0e1696a6
Dandymay <dandyainul19@gmail.com>
2024-04-30 10:09:42 +0700

    first commit

---
+++ src/components/atoms/Category/index.tsx
@@ -0,0 +1,21 @@
+import { FC, MouseEventHandler } from "react";
+import Image from "next/image";
+
+interface CategoryPropType {
+  imgUrl: string;
+  imgAlt: string;
+  text: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Category: FC<CategoryPropType> = (props) => {
+  const { imgUrl, imgAlt, text, onClick } = props;
+  return (
+    <button className="flex flex-col gap-4 items-center" onClick={onClick}>
+      <Image height={101} width={105} src={imgUrl} alt={imgAlt} />
+      <p>{text}</p>
+    </button>
+  );
+};
+
+export default Category;
